a0a5d994d812b24fef45f41cb5c3097305bc935f
jianfeng <497609288@qq.com>
2018-11-09 11:30:55 +0800

    new

---
+++ WisdomScanKitDemo/WisdomScanKitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WisdomScanKitDemo
+//
+//  Created by jianfeng on 2018/11/9.
+//  Copyright © 2018年 All over the sky star. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
