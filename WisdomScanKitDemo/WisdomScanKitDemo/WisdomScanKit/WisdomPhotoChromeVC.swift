181f176e8c4183fae42f8e166ceb5121be1fbf21
jianfeng <497609288@qq.com>
2018-11-24 20:38:56 +0800

    chrome

---
+++ WisdomScanKitDemo/WisdomScanKitDemo/WisdomScanKit/WisdomPhotoChromeVC.swift
@@ -0,0 +1,18 @@
+//
+//  WisdomPhotoChromeVC.swift
+//  WisdomScanKitDemo
+//
+//  Created by jianfeng on 2018/11/24.
+//  Copyright © 2018年 All over the sky star. All rights reserved.
+//
+
+import UIKit
+
+class WisdomPhotoChromeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
