7b20cad0e8a2107e69630342d22c9e202cc2afb4
tangjianfeng <497609299@qq.com>
2022-10-27 00:19:06 +0800

    add

--- Source/Photo/View/WisdomPhotoSelectCell.swift
+++ Source/Photo/View/WisdomPhotoSelectCell.swift
@@ -13,24 +13,13 @@ import Photos
 class WisdomPhotoBaseCell: UICollectionViewCell {
     
     private lazy var imageView: UIImageView = {
-        let view = UIImageView(frame: self.contentView.bounds)
+        let view = UIImageView(frame: contentView.bounds)
         view.backgroundColor = UIColor.clear
         view.contentMode = .scaleAspectFill
         view.clipsToBounds = true
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
-    
-//    private(set) lazy var selectBtn: UIButton = {
-//        let btn = UIButton(frame: CGRect(x: self.contentView.bounds.width - 23, y: 2, width: 21, height: 21))
-//        var image = WisdomScanManager.bundleImage(name: "shan_element_book")
-//        btn.setBackgroundImage(image, for: .normal)
-//        image = WisdomScanManager.bundleImage(name: "selectedbackgroud_iconNew")
-//        btn.setBackgroundImage(image, for: .selected)
-//        btn.addTarget(self, action: #selector(clickSelectedBtn), for: .touchUpInside)
-//        btn.titleLabel?.font = UIFont.systemFont(ofSize: 13)
-//        return btn
-//    }()
     
     public var image: UIImage? {
         didSet{
@@ -42,6 +31,10 @@ class WisdomPhotoBaseCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        backgroundColor  = UIColor.clear
+        
+        contentView.backgroundColor = UIColor.clear
+        
         contentView.addSubview(imageView)
         
         contentView.wisdom_addConstraint(with: imageView,
@@ -55,15 +48,7 @@ class WisdomPhotoBaseCell: UICollectionViewCell {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-//    @objc private func clickSelectedBtn(btn: UIButton){
-//        if hander != nil {
-//            let res = hander!(btn.isSelected,image!)
-//            btn.isSelected = res
-//        }
-//    }
 }
-
 
 
 class WisdomPhotoSelectCell: UICollectionViewCell {
